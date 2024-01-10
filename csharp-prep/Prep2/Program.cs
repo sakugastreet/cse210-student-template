77e307b80b820e577710b745363206d353affa39
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2024-01-09 20:56:27 -0700

    prep 2 Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade Percentage? ");
+        string answer = Console.ReadLine();
+
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if(percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck Next Time!");
+        }
+
     }
 }
