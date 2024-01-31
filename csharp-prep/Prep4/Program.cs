de2432216db7e553a5bccf140238af0680726114
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2024-01-30 21:55:27 -0700

    prep 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber;
+        Console.WriteLine("Please enter a number(enter 0 if your done with life and don't want to live anymore)");
+        
+        do
+        {
+            Console.Write("Enter Number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+
+
+        } while (newNumber != 0);
+
+        int listSum = 0;
+        int list_largest = 0;
+        foreach (int number in numbers)
+        {
+            listSum += number;
+            if (number > list_largest)
+            {
+                list_largest = number;
+            }
+        }
+        Console.WriteLine($"the Sum of all the numbers you entered is {listSum}");
+        Console.WriteLine($"the average of all the numbers you entered is {listSum / numbers.Count}");
+        Console.WriteLine($"the largest of all the numbers you entered is {list_largest}");
+
+
     }
 }
