c0f42a705d2ebdd48309255555316af65f3148bc
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2024-01-30 22:16:42 -0700

    prep five completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
+    {
+        DisplayWelcomeMessage();
+        string username = PromptUserName();
+        int numberUserLike = PromptUserNumber();
+        int squarednumber = SquareNumber(numberUserLike);
+        DiplayResult(username, squarednumber);
+    }
+    static void DisplayWelcomeMessage()
     {
         Console.WriteLine("Hello Prep5 World!");
+
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your full name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("My Good Sir, what is your favorite number? ");
+        int favorite_number = int.Parse(Console.ReadLine());
+
+        return favorite_number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        number = number * number;
+
+        return number;
+    }
+
+    static void DiplayResult(string username, int number)
+    {
+        Console.WriteLine($"The user's name is {username} and their number squared is {number}.");
     }
 }
