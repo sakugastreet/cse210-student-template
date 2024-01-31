f50ed0174530453abcfb55b4301fbfdf11d135d6
Joseph Earl <78830164+sakugastreet@users.noreply.github.com>
2024-01-30 20:42:27 -0700

    prep 3 work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Net;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+
+
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
-}
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int userGuess;
+        int guessAmount = 0;
+
+        do
+        {
+            guessAmount += 1;
+            Console.Write("Please enter a guess: ");
+            userGuess = int.Parse(Console.ReadLine());
+            if (userGuess == magicNumber)
+            {
+                Console.WriteLine($"Gongratulations! You got it in {guessAmount}");
+            }
+            else if (userGuess >= magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Higher!");
+            }
+
+
+    
+        } while (userGuess != magicNumber);
+
+
+
+        }  
+    }  
+    
